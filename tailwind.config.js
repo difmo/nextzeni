22af0e01410c94b41b44857d09a6b35ac449a1e6
amarcodeservir <amarcodeservir@gmail.com>
2025-02-04 18:13:31 +0530

    change

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,8 @@ module.exports = {
         "black-100": "#100d25",
         "black-200": "#090325",
         "white-100": "#f3f3f3",
-        "mybg": "#25293c",
-        "halkabg": "#f3f3f3",
+        mybg: "#25293c",
+        halkabg: "#f3f3f3",
       },
       boxShadow: {
         card: "0px 35px 120px -15px #211e35",
